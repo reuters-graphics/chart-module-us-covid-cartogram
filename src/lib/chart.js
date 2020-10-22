0ce016e2bca45b5ad4cb2fe71e5d617906abcb85
Jon McClure <jon.r.mcclure@gmail.com>
2020-10-22 20:04:17 +0100

    try bundling without d3-appendselect

--- src/lib/chart.js
+++ src/lib/chart.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import 'd3-appendselect';
 
 import * as d3 from 'd3';
@@ -13,7 +14,7 @@ class USStateCartogram extends ChartComponent {
     avg_days: 7,
     bars: true,
     paddingX: 5,
-    paddingY: 5, 
+    paddingY: 5,
     mobileWidth: 650,
 
     uniformScale: false,
@@ -39,14 +40,14 @@ class USStateCartogram extends ChartComponent {
       'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY',
     ],
 
-    stateGridLookup: { '7-1': 'AK', '6-6': 'AL', '5-4': 'AR', '5-1': 'AZ', '4-0': 'CA', '4-2': 'CO', '3-9': 'CT', '5-10': 'DC', '4-9': 'DE', '7-8': 'FL', '6-7': 'GA', '7-0': 'HI', '3-4': 'IA', '2-1': 'ID', '2-5': 'IL', '3-5': 'IN', '5-3': 'KS', '4-5': 'KY', '6-4': 'LA', '2-9': 'MA', '4-8': 'MD', '0-10': 'ME', '2-6': 'MI', '2-4': 'MN', '4-4': 'MO', '6-5': 'MS', '2-2': 'MT', '5-6': 'NC', '2-3': 'ND', '4-3': 'NE', '1-10': 'NH', '3-8': 'NJ', '5-2': 'NM', '3-1': 'NV', '2-8': 'NY', '3-6': 'OH', '6-3': 'OK', '3-0': 'OR', '3-7': 'PA', '3-10': 'RI', '5-7': 'SC', '3-3': 'SD', '5-5': 'TN', '7-3': 'TX', '4-1': 'UT', '4-7': 'VA', '1-9': 'VT', '2-0': 'WA', '1-5': 'WI', '4-6': 'WV', '3-2': 'WY', '7-10': 'PR', },
+    stateGridLookup: { '7-1': 'AK', '6-6': 'AL', '5-4': 'AR', '5-1': 'AZ', '4-0': 'CA', '4-2': 'CO', '3-9': 'CT', '5-10': 'DC', '4-9': 'DE', '7-8': 'FL', '6-7': 'GA', '7-0': 'HI', '3-4': 'IA', '2-1': 'ID', '2-5': 'IL', '3-5': 'IN', '5-3': 'KS', '4-5': 'KY', '6-4': 'LA', '2-9': 'MA', '4-8': 'MD', '0-10': 'ME', '2-6': 'MI', '2-4': 'MN', '4-4': 'MO', '6-5': 'MS', '2-2': 'MT', '5-6': 'NC', '2-3': 'ND', '4-3': 'NE', '1-10': 'NH', '3-8': 'NJ', '5-2': 'NM', '3-1': 'NV', '2-8': 'NY', '3-6': 'OH', '6-3': 'OK', '3-0': 'OR', '3-7': 'PA', '3-10': 'RI', '5-7': 'SC', '3-3': 'SD', '5-5': 'TN', '7-3': 'TX', '4-1': 'UT', '4-7': 'VA', '1-9': 'VT', '2-0': 'WA', '1-5': 'WI', '4-6': 'WV', '3-2': 'WY', '7-10': 'PR' },
   };
 
   draw() {
     const downArrow = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" class="svg-inline--fa fa-caret-down fa-w-10 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path></svg>';
     const upArrow = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-up" class="svg-inline--fa fa-caret-up fa-w-10 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"></path></svg>';
     const props = this.props();
-    if (props.parameter==='cases' || props.parameter==='deaths') {
+    if (props.parameter === 'cases' || props.parameter === 'deaths') {
       const data = this.data()[0];
       const node = this.selection().node();
       let max = 0;
@@ -58,7 +59,7 @@ class USStateCartogram extends ChartComponent {
         }
         data.states[key].avg = [];
         data.states[key][props.parameter].reverse().forEach(function(d, i) {
-          data.states[key].avg.push(d3.mean(data.states[key][props.parameter].slice(i, (i + props.avg_days)), d => d < 0 ? 0 : d))
+          data.states[key].avg.push(d3.mean(data.states[key][props.parameter].slice(i, (i + props.avg_days)), d => d < 0 ? 0 : d));
         });
         data.states[key].avg.reverse();
         data.states[key][props.parameter].reverse();
@@ -94,7 +95,7 @@ class USStateCartogram extends ChartComponent {
       const grid = createGrid();
       let rows = Math.floor(grid.length / divisor) + 1;
       if (width < props.mobileWidth) {
-        rows = rows+1
+        rows = rows + 1;
       }
       const smallH = (props.height - props.margin.top) / (rows - 1);
 
@@ -105,7 +106,7 @@ class USStateCartogram extends ChartComponent {
       const scaleY = d3.scaleLinear();
       if (props.uniformScale) {
         scaleY.domain([0, max])
-          .range([smallH * 0.95, -smallH * -0.6])
+          .range([smallH * 0.95, -smallH * -0.6]);
       } else {
         scaleY.domain([0, 1])
           .range([smallH * 0.95, smallH * 0.25]);
@@ -113,7 +114,7 @@ class USStateCartogram extends ChartComponent {
 
       const line = d3.line()
         .x((d, i) => scaleXTime(data.series[i]))
-        .y((d, i) => scaleY(d ? d : 0))
+        .y((d, i) => scaleY(d || 0))
         .curve(d3.curveMonotoneX);
 
       const area = d3.area()
@@ -128,26 +129,26 @@ class USStateCartogram extends ChartComponent {
         .attr('height', props.height)
         .appendSelect('g')
         .attr('transform', `translate(${props.margin.left}, ${props.margin.top})`);
-  
+
       const statesG = g.appendSelect('g.states-g-group')
         .selectAll('.state')
         .data(props.stAbbr, d => d);
-  
+
       const stateGCon = statesG
         .enter()
         .appendSelect('g')
         .attr('class', (st) => {
           return `state ${st}`;
         })
-        .merge(statesG)
-  
+        .merge(statesG);
+
       stateGCon
         .attr('transform', (st) => {
           const left = statePosition[st].column * smallW;
           const top = statePosition[st].row * smallH;
           return `translate(${left}, ${top})`;
         });
-  
+
       stateGCon.appendSelect('path.area')
         .style('fill', props.fill)
         .attr('d', (d) => {
@@ -157,7 +158,7 @@ class USStateCartogram extends ChartComponent {
             return area(data.states[d][props.parameter].map(e => e / data.states[d].max));
           }
         });
-  
+
       stateGCon.appendSelect('path.line')
         .style('stroke', props.stroke)
         .style('stroke-width', props.strokeWidth)
@@ -169,22 +170,22 @@ class USStateCartogram extends ChartComponent {
             return line(data.states[d].avg.map(e => e / data.states[d].max));
           }
         });
-  
+
       statesG.exit()
         .transition(transition)
         .remove();
-  
+
       const stateNames = this.selection()
         .appendSelect('div.name-container')
         .selectAll('.state-name')
         .data(props.stAbbr);
-  
+
       stateNames.enter()
         .appendSelect('div.state-name')
         .merge(stateNames)
         .each(function(d) {
-          if (data.states[d].percentOfPeak[props.parameter]>=.9) {
-            d3.select(this).classed('bold', true)
+          if (data.states[d].percentOfPeak[props.parameter] >= 0.9) {
+            d3.select(this).classed('bold', true);
           }
         })
         .style('top', function(d) {
@@ -196,7 +197,7 @@ class USStateCartogram extends ChartComponent {
           return `${left}px`;
         })
         .html(d => `${getArrow(data.states[d].avg)} <p>${data.states[d].stateAP}</p>`);
-  
+
       function getArrow(numbers) {
         const latest = numbers[numbers.length - 1];
         const previousWeek = numbers[numbers.length - 8];
@@ -209,30 +210,30 @@ class USStateCartogram extends ChartComponent {
           return '';
         }
       }
-  
+
       function createGrid() {
         const gridArr = [];
         let row = -1;
-        console.log(width)
+        console.log(width);
         if (width >= props.mobileWidth) {
           for (let i = 0; i < 88; i++) {
             const column = i % 11;
             row = column === 0 ? row + 1 : row;
-  
+
             const obj = {
               row: row,
               column: column,
-              st: props.stateGridLookup[`${row}-${column}`]
-                ? props.stateGridLookup[`${row}-${column}`]
-                : null,
+              st: props.stateGridLookup[`${row}-${column}`] ?
+                props.stateGridLookup[`${row}-${column}`] :
+                null,
             };
-  
+
             gridArr.push(obj);
-  
-            const st = props.stateGridLookup[`${row}-${column}`]
-              ? props.stateGridLookup[`${row}-${column}`]
-              : null;
-  
+
+            const st = props.stateGridLookup[`${row}-${column}`] ?
+              props.stateGridLookup[`${row}-${column}`] :
+              null;
+
             if (st) {
               statePosition[st] = obj;
             }
@@ -241,21 +242,22 @@ class USStateCartogram extends ChartComponent {
           for (var i = 0; i < props.stAbbr.length; i++) {
             const column = i % divisor;
             row = column == 0 ? row + 1 : row;
-  
+
             const obj = {
               row: row,
               column: column,
               st: props.stAbbr[i],
             };
-  
+
             gridArr.push(obj);
-  
+
             statePosition[props.stAbbr[i]] = obj;
           }
         }
         return gridArr;
       }
-      return this;}
+      return this;
+    }
   }
 }
 
